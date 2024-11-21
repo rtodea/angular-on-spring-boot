0fa7d9e91225b984f45241fa176c1d2b3165ac96
Robert Todea <rtodea@mobileinsight.com>
2024-11-21 06:13:52 +0200

    Add @Autowired annotation for UserRepository in UserController
    The @Autowired annotation is introduced for the UserRepository to support dependency injection, eliminating the need for a no-arg constructor. This change enhances the code by enforcing proper dependency management and simplification.

--- server/src/main/java/com/example/demo/controller/UserController.java
+++ server/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,9 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
+    @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
-    }
-
-    public UserController() {
-        this.userRepository = null;
     }
 
     private final UserRepository userRepository;
